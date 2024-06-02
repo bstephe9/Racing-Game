4263f2ab58dd16437e5b9e1b9c8329e2b285653f
bstephe9 <tun78906@temple.edu>
2024-06-02 01:20:53 -0400

    feat: Add road, rotation

--- player/player.gd
+++ player/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody3D
 
-const SPEED = 5.0
+const SPEED = 20.0
+const ROTATE_SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -12,18 +13,24 @@ func _physics_process(delta: float) -> void:
 		velocity.y -= gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("hop") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("move_left", "move_right", "none", "none")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir := Input.get_vector("turn_left", "turn_right", "move_forward", "none")
+	var direction := (transform.basis * Vector3(0, 0, input_dir.y)).normalized()
+	
+	print("%s | %s" % [input_dir, direction])
+	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	rotate_y(-deg_to_rad(input_dir.x * ROTATE_SPEED))
+	#$Model.rotation.y = clamp($Model.rotation.y, 0, PI/2)
 
 	move_and_slide()

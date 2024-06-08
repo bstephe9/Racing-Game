a0acdf3b77ee3d64e42825e6ee317bcb2f2ce847
bstephe9 <tun78906@temple.edu>
2024-06-08 18:20:21 -0400

    feat: Add lap number in UI, update it when start line is passed

--- level/level.gd
+++ level/level.gd
@@ -1,14 +1,20 @@
 extends Node3D
 
+const MAX_LAPS: int = 3
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var current_lap: int = 1
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	update_ui()
+
+func debug() -> void:
 	$Debug/L_car.text = "Pos: %s | Rot: %s" % [
 		$Vehicle.position,
 		$Vehicle.rotation
 	]
+
+func update_ui() -> void:
+	$UI/Lap.text = "Lap: %d/%d" % [current_lap, MAX_LAPS]
+
+func _on_start_line_body_entered(body: Node3D) -> void:
+	current_lap += 1

5c94452320b34b106cf3d8db9b696c8ad26427b0
bstephe9 <tun78906@temple.edu>
2024-06-12 17:24:01 -0400

    feat: Make camera chase vehicle

--- vehicle/camera/camera_pivot.gd
+++ vehicle/camera/camera_pivot.gd
@@ -1,7 +1,13 @@
 extends Node3D
 
+@onready var parent = self.owner
+
 func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_pressed("look_behind"):
 		$RearCamera.make_current()
 	else:
 		$ForwardCamera.make_current()
+
+func _physics_process(delta: float) -> void:
+	global_position = global_position.lerp(parent.global_position, delta * 20.0)
+	transform = transform.interpolate_with(parent.transform, delta * 5.0)
